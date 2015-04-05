cc596e726156c73ccbc9b3793978170ec121f160
Marco Heumann (mheumann) <marco.heumann@web.de>
2015-04-05 11:24:27 +0200

    added config file for travis

--- api/DB.class.php
+++ api/DB.class.php
@@ -1,5 +1,5 @@
 <?php
-if(preg_match('~*travis*~' ,filter_input(INPUT_SERVER, 'HTTP_HOST'))) {
+if(preg_match('~(.*)(travis)(.*)~' ,filter_input(INPUT_SERVER, 'HTTP_HOST'))) {
     require_once __DIR__ . '/config-travis.inc';
 } else {
     require_once __DIR__ . '/config.inc';
